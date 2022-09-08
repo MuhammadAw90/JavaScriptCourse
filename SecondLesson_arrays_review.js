9aa1ef7d25fdd897e2d2b0d43cd8e13e16b6ae64
Muhammad Awawdy <muhammad.awawdy90@gmail.com>
2022-09-09 00:30:18 +0300

    finishing arrays

---
+++ SecondLesson_arrays_review.js
@@ -0,0 +1,15 @@
+// JavaScript Arrays: Review
+// It's time to review! These examples are all taken from lessons that you've finished. Revisiting them makes sure that you haven't forgotten anything.
+// Type in what each expression will evaluate to. For example, for the prompt 1 + 1, you would type 2.
+
+const strings = ['a', 'b', 'c'];
+strings[1] = 'x';
+strings;
+//RESULT:
+//['a', 'x', 'c']
+
+['a', 'b', 'c'][1];
+//RESULT:
+//'b'
+//LESSON:
+//Basics
