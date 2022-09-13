ee516daf491f5ed6fe6cd2576854ec03c0a8994d
Muhammad Awawdy <muhammad.awawdy90@gmail.com>
2022-09-13 18:40:14 +0300

    Seventh Review

---
+++ Seventh_Review_Slice.js
@@ -0,0 +1,11 @@
+// JavaScript Arrays: Review
+// It's time to review! These examples are all taken from lessons that you've finished. Revisiting them makes sure that you haven't forgotten anything.
+// Type in what each expression will evaluate to. For example, for the prompt 1 + 1, you would type 2.
+
+[true, false, true, false].slice(2000000, 3000000);
+
+// RESULT:
+// []
+
+// LESSON:
+// Slice
